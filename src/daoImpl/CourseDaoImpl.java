8a5b54805f94c0a1e89284cd167f89fcd146d3c9
liuy <908657326@qq.com>
2020-07-02 09:58:38 +0800

    cc

--- src/daoImpl/CourseDaoImpl.java
+++ src/daoImpl/CourseDaoImpl.java
@@ -1,6 +1,5 @@
 package daoImpl;
 
-import java.awt.datatransfer.StringSelection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,7 +14,7 @@ import dao.CourseDao;
 
 public class CourseDaoImpl implements CourseDao {
 	//根据teacher id查看该老师所带所有班级
-	public List<String> GetClassByTrId(String teacher_id) {
+	public List<String> GetClassByTrId(int teacher_id) {
 		// TODO Auto-generated method stub
 		Connection conn=DBUtils.getConnection();
 		String sql="select course_class from course where teacher_id="+teacher_id;
@@ -41,63 +40,13 @@ public class CourseDaoImpl implements CourseDao {
 		}
 		return list;
 	}
-	//获取当前假期的开学日期
-	public String getTermStart() {
-	// TODO Auto-generated method stub
-		Connection conn=DBUtils.getConnection();
-		String sql="select max(date) max_date from holiday where holiday.desc ='开学'";
-		PreparedStatement st=null;
-		ResultSet rs=null;
-		String str="";
-		try {
-			st=conn.prepareStatement(sql);
-			rs=st.executeQuery();
-			if(rs.next()) {
-				str=rs.getString("max_date");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			DBUtils.close(rs, st, conn);
-		}	
-		return str;
-    }
+	
 	//根据teacher id查看该老师所有课程信息
-		public List<course> FindCouserBuTrId(String teacher_id) {
+		public List<course> FindCouserBuTrId(int teacher_id) {
 			// TODO Auto-generated method stub
 			Connection conn=DBUtils.getConnection();
 			String sql="select c.*,t.name from course c,teacher t where c.teacher_id=t.id and t.id="+teacher_id;
-			PreparedStatement st=null;
-			ResultSet rs=null;
-			List<course> list=new ArrayList<course>();
-			try {
-				st=conn.prepareStatement(sql);
-				rs=st.executeQuery();
-				while(rs.next()) {
-					course c=new course();
-					c.setCourse_class(rs.getString("course_class"));
-					c.setCourse_hours(rs.getInt("course_hours"));
-					c.setCourse_id(rs.getInt("course_id"));
-					c.setCourse_name(rs.getString("course_name"));
-					c.setCourse_place(rs.getString("course_place"));
-					c.setCourse_week(rs.getString("course_week"));
-					c.setCouser_peoplenum(rs.getInt("course_peoplenum"));
-					c.setTeacher_id(rs.getInt("teacher_id"));
-					c.setTeachr_name(rs.getString("name"));
-					c.setTerm_start(rs.getString("term_start"));
-					c.setWeek(rs.getString("week"));
-					list.add(c);
-					
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}finally {
-				DBUtils.close(rs, st, conn);
-			}
-			
-			return list;
+			return null;
 		}
 	//查看所有课程信息
 	public List<course> GetAll() {
@@ -106,16 +55,9 @@ public class CourseDaoImpl implements CourseDao {
 	}
 public static void main(String[] args) {
 	CourseDaoImpl daoImpl=new CourseDaoImpl();
-	/*List<String> list=daoImpl.GetClassByTrId("1");
-	for(int i=0;i<list.size();i++) {
-		System.out.println(list.get(i));
-	}*/
-	//System.out.println(daoImpl.getTermStart());
-	List<course> list=daoImpl.FindCouserBuTrId("1");
+	List<String> list=daoImpl.GetClassByTrId(1);
 	for(int i=0;i<list.size();i++) {
 		System.out.println(list.get(i));
 	}
 }
-
-
 }
--- src/servlet/GetCourseServlet.java
+++ src/servlet/GetCourseServlet.java
@@ -1,26 +1,15 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jdt.internal.compiler.lookup.VariableBinding;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import bean.course;
-import daoImpl.CourseDaoImpl;
-
 /**
  * Servlet implementation class GetCourseServlet
- * 获取该老师课程表信息
+ * 获取课程表信息
  */
 @WebServlet("/course")
 public class GetCourseServlet extends HttpServlet {
@@ -42,18 +31,6 @@ public class GetCourseServlet extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
-		//教师id
-		String teacher_id=request.getParameter("teacher_id");
-		
-		CourseDaoImpl daoImpl=new CourseDaoImpl();
-		List<course> list=new ArrayList<course>();
-		list=daoImpl.FindCouserBuTrId(teacher_id);
-		ObjectMapper mapper=new ObjectMapper();
-		String data=mapper.writeValueAsString(list);
-		PrintWriter pWriter=response.getWriter();
-		pWriter.println(data);
-		pWriter.flush();
-		pWriter.close();
 	}
 
 	/**
